18c8dd69891718ba04a585d4e682bf7acba41a59
Mathew Griesbach <mathew.griesbach@gmail.com>
2017-05-28 12:20:25 -0700

    class notes

--- Class.playground/Contents.swift
+++ Class.playground/Contents.swift
@@ -1,5 +1,32 @@
-//: Playground - noun: a place where people can play
+//Arrays Class Notes
 
-import UIKit
+var FavCandy = ["Kit Kat", "Hershey", "Reeses Pieces", "Reeses Cup"]
 
-var str = "Hello, playground"
+FavCandy.append("Fun Dip")
+
+//Indexes Class Notes
+
+var favNumbers = [65, 77, 21, 99]
+
+favNumbers [0]
+
+favNumbers [3]
+
+favNumbers [1] = 3
+
+favNumbers
+
+favNumbers.insert(33, at: 2)
+
+
+var Numbers = [1, 2, 4, 5]
+
+Numbers.insert(3, at: 2)
+
+
+
+
+
+
+
+
